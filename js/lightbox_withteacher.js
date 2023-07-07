cada176c2e68c30c9c9da7072ec4ffeef8e3f1a8
polkmn0995 <polkmn0995@gmail.com>
2023-07-07 16:31:09 +0900

    no message

--- js/lightbox_withteacher.js
+++ js/lightbox_withteacher.js
@@ -1,12 +1,15 @@
 var pics = document.querySelectorAll(".pic");
-var lightBox = document.querySelector("#lightbox");
-var lightBoxImage = document.querySelector("#lightboxImage");
+var lightbox = document.querySelector("#lightbox");
+var lightboxImage = document.querySelector("#lightboxImage");
 
 for (var i = 0; i < pics.length; i++) {
   pics[i].addEventListener("click", showLightBox);
 }
 function showLightBox() {
   var bigLocation = this.getAttribute("data-src");
-  lightBoxImage.setAttribute("src", bigLocation);
-  lightBox.style.display = "block";
+  lightboxImage.setAttribute("src", bigLocation);
+  lightbox.style.display = "block";
 }
+lightbox.onclick = function () {
+  lightbox.style.display = "none";
+};
